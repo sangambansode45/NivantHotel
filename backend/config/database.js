9e8452245187150d7214066f56a8aa7ad96966e7
sangambansode45 <bansodesangam23@gmail.com>
2026-03-03 20:46:00 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   // 🔥 ADD THIS LINE
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
